4ef24f0e5f75ce4c54efbf9d741d4041ffe3bb37
kar-cloud <karanmann1545@gmail.com>
2021-08-06 12:42:37 +0530

    deployed verion-3

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ app.post("/user/data/delete", verifyToken, async (req, res) => {
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
